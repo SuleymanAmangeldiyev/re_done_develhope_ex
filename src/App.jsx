574b4468b339c034414ecc4a062153f15bfbddae
Suleyman <suleyman20071998@gmail.com>
2022-11-26 21:08:51 +0200

    the ex: 04 (components - 01) has done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import React from 'react'
 
 
-function add(a, b){
-  return <h2>"The sum of {a} + {b} = {a + b}"</h2>
-}
 
-function App() {
- 
-  return (
-    <div>
-     <h1>{add(10, 5)}</h1>
-    </div>
-  );
-}
 
-export default App;
 
-// the ex: 02 (jsx-05)
 
-// Write a function that receives two parameters, a and b and returns the sum of them within an h2 tag.
+
+
+
+
+
+
+
+
+// the ex: 04 (Components - 01)
+
+// Create a new React application with create-react-app and remove all the code within the src folder.
