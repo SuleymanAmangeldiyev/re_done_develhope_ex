2bc58bdd6feced2c76ab25c2e24e10ed30ee7671
Suleyman <suleyman20071998@gmail.com>
2022-11-26 20:52:11 +0200

    the first ex has done (jsx-01)

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+ 
+ const hello = '"Hello, World!".' 
+ 
+  return (
+    <div>
+     <h1>{hello}</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// the ex: 01 (jsx-01)
+
+// Create a variable called hello and inizialize it with an h1 tag with the message "Hello, World!".
