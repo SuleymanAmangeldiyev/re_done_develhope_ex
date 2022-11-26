10cadb4d9e539c353f723ad2a84db7abbbf65cfa
Suleyman <suleyman20071998@gmail.com>
2022-11-26 23:28:54 +0200

    the ex 20 (state - 01) has done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import Welcome from './Welcome'
+import Counter from './Counter'
 export class App extends Component {
   
   render() {
   const ady = "John"
     return (
-      <div><Welcome name={ady}  age='4' /></div>
+      <div>
+        <Counter />
+      </div>
     )
   }
 }
@@ -14,6 +16,6 @@ export default App
 
 
 
-// the ex: 19 (Conditional Rendering - 05)
+// the ex: 20 (state - 01)
 
-// Modify the Age component so that the "Your age is " message is rendered only if the age prop is greater than 18. Otherwise render the "You are very young!" message.
+// Create a Counter class component with an internal state containing a count property, initialized to 0. The Counter component should render the count property within an h1 tag, and the count property should be incremented by 1 every second;
---
+++ src/Counter.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export class Counter extends Component {
+ state = {
+  count: 0
+ }
+  
+ constructor(props) {
+    super(props)
+   setInterval(() => {
+     this.setState({
+       count: this.state.count + 1,
+     });
+   }, 1000);
+  }
+
+  
+ 
+  render() {
+    return (
+      <h1>{this.state.count}</h1>
+    )
+  }
+}
+
+export default Counter
