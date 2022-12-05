4c7df4c8c4b870bb13c1c2ce0c6aab1367d5e437
Suleyman <suleyman20071998@gmail.com>
2022-12-05 16:29:34 +0200

    the ex: (Function Components - 03) has done

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Sum from "./Sum";
 export class App extends Component {
   render() {
     
- const nums = [12, 32, 32, 21, 423, 21]
+
     return (
       <div>
-    <Sum numbers={nums} />       
+    <Sum  />       
        
       </div>
     );
@@ -15,6 +15,4 @@ export class App extends Component {
 
 export default App;
 
-// the ex: 43  (Function Components - 01)
-
-//Write a Sum function component that receives a numbers prop (expected to be an array of numbers) and renders the sum of all numbers in the array within a h1 tag.
+// the ex: (Function Components - 03)
--- src/Sum.jsx
+++ src/Sum.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-export default function Sum({numbers}) {
-  return <h1>{numbers.reduce((a, b) => a + b )}</h1>;
+export default function Sum({ numbers = [12, 32, 32, 21, 423, 21] }) {
+  return <h1>{numbers.reduce((a, b) => a + b)}</h1>;
 }
